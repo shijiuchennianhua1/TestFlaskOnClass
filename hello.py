67f8ad135450fcbe5f401739de22b5ac07054279
201841030128sbw <user@Mac39deiMac.local>
2020-11-09 09:40:56 +0800

    2020-11-9 添加了数据库

--- hello.py
+++ hello.py
@@ -4,14 +4,34 @@ from flask_moment import Moment
 from datetime import datetime
 from form import NameForm
 from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = '*^*9595∑åß∂'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chenxuan:zhimakaimen@127.0.0.1/pblog'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    user = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/', methods=['POST','GET'])
 @app.route('/index', methods=['POST','GET'])
@@ -20,13 +40,20 @@ def index():
     response.set_cookie('answer', '42')
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data, role=Role.query.filter_by(name='user').first())
+            db.session.add(user)
+            db.session.commit()
+            # 表明是新注册的用户
+            session['know'] = False
+        else:
+            session['know'] = True
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', content='sadadas', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    return render_template('index.html', content='sadadas', current_time=datetime.utcnow(), form=form, 
+    name=session.get('name'), know=session.get('know', False))
 
 @app.route('/user/<string:username>')
 def user(username):
@@ -46,3 +73,7 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     app.run(debug=1, port=5000)
+
+@app.shell_context_processor 
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
